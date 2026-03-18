e3f218859740e0d7739b8b596c16c1113105243a
Angelo Dylan <angelo.quispe.l@tecsup.edu.pe>
2026-03-18 14:15:40 -0500

    Feat: Crear el SauceLabsStepDef con metodos sin implementacion de logica

---
+++ src/test/java/SauceLabsStepDef.java
@@ -0,0 +1,31 @@
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class SauceLabsStepDef {
+    @Given("estoy en la aplicación de SauceLabs")
+    public void estoyEnLaAplicaciónDeSauceLabs() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("valido que carguen correctamente los productos en la galeria")
+    public void validoQueCarguenCorrectamenteLosProductosEnLaGaleria() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("agrego <UNIDADES> del siguiente producto {string}")
+    public void agregoUNIDADESDelSiguienteProducto(String arg0) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("valido el carrito de compra actualice correctamente")
+    public void validoElCarritoDeCompraActualiceCorrectamente() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
